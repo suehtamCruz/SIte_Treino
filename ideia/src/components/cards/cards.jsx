3eb1e4cee3f0d78492ea9dadfdc9f800e396bb2b
Matheus Cruz <matheuscz3110@gmail.com>
2020-10-10 21:12:45 -0300

    card criado

---
+++ ideia/src/components/cards/cards.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+import './styleCard.css';
+
+class Card extends Component{
+    constructor(props){
+        super(props);
+        
+    }
+    render(){
+        return(
+            <>
+
+            </>
+        );
+    }
+}
--- ideia/src/components/content/content.jsx
+++ ideia/src/components/content/content.jsx
@@ -1,14 +1,33 @@
 import React from 'react';
 import { Component } from 'react';
 
+import Car from '../../assets/car.jpg'
+import Carro from '../../assets/carro.jpg'
+
+
 import './styleContent.css';
 class Content extends Component{
     render(){
         return (
             <>
                 <section className="content">
-                    <h1 className="title">Copper é uma empresa destinada a fazera vontade dos seus clientes</h1>
-                    
+                    <h1 className="title">Copper é uma empresa destinada a fazer a vontade dos seus clientes</h1>
+                    <div className="card">
+                        <img className="card_img" src={Car} alt="img carro card" />
+                        <div className="card_line" />
+                        <p className="card_text">Carro modelo tal top maravilhoso</p>
+                        <a href="https://renegade.jeep.com.br/#/" className="card_button">
+                            <p className="button_text">ADQUIRIR</p>
+                        </a>
+                    </div>
+                    <div className="card">
+                        <img className="card_img" src={Carro} alt="img carro card 2 " />
+                        <div className="card_line" />
+                        <p className="card_text">Carro modelo tal top maravilhoso</p>
+                        <a href="https://renegade.jeep.com.br/#/" className="card_button">
+                            <p className="button_text">ADQUIRIR</p>
+                        </a>
+                    </div>
                 </section>  
             </>
         );
