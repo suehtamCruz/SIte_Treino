64bf566e2c46179d09136c631a81214c925e254f
Matheus Cruz <matheuscz3110@gmail.com>
2020-10-10 23:29:00 -0300

    card component created

--- ideia/src/components/cards/cards.jsx
+++ ideia/src/components/cards/cards.jsx
@@ -3,15 +3,24 @@ import React, { Component } from 'react';
 import './styleCard.css';
 
 class Card extends Component{
-    constructor(props){
-        super(props);
+    // constructor(props){
+    //     super(props);
         
-    }
+    // }
     render(){
         return(
             <>
-
+                 <div className="card">
+                        <img className="card_img" src={this.props.img} alt="img carro card" />
+                        <div className="card_line" />
+                        <p className="card_text">{this.props.text}</p>
+                        <a href={this.props.href} className="card_button">
+                            <p className="button_text">ADQUIRIR</p>
+                        </a>
+                </div>
             </>
         );
     }
 }
+
+export default Card;
--- ideia/src/components/content/content.jsx
+++ ideia/src/components/content/content.jsx
@@ -4,6 +4,8 @@ import { Component } from 'react';
 import Car from '../../assets/car.jpg'
 import Carro from '../../assets/carro.jpg'
 
+import Card from '../cards/cards';
+
 
 import './styleContent.css';
 class Content extends Component{
@@ -12,22 +14,10 @@ class Content extends Component{
             <>
                 <section className="content">
                     <h1 className="title">Copper é uma empresa destinada a fazer a vontade dos seus clientes</h1>
-                    <div className="card">
-                        <img className="card_img" src={Car} alt="img carro card" />
-                        <div className="card_line" />
-                        <p className="card_text">Carro modelo tal top maravilhoso</p>
-                        <a href="https://renegade.jeep.com.br/#/" className="card_button">
-                            <p className="button_text">ADQUIRIR</p>
-                        </a>
-                    </div>
-                    <div className="card">
-                        <img className="card_img" src={Carro} alt="img carro card 2 " />
-                        <div className="card_line" />
-                        <p className="card_text">Carro modelo tal top maravilhoso</p>
-                        <a href="https://renegade.jeep.com.br/#/" className="card_button">
-                            <p className="button_text">ADQUIRIR</p>
-                        </a>
-                    </div>
+                   
+                    <Card img={Car} text="Carro modelo tal top maravilhoso" href="https://renegade.jeep.com.br/#/" />
+                    <Card img={Carro} text="Carro modelo tal top maravilhoso" href="https://renegade.jeep.com.br/#/" />
+                   
                 </section>  
             </>
         );
